bc7c260c92366c38173f18ce34f84afc9988fddd
Nicolas Luck <nicolas@lucksus.eu>
2025-05-26 16:07:19 +0200

    Add new build files, fell through ignore pattern

---
+++ lib/utils/cell.d.ts
@@ -0,0 +1,11 @@
+import { CellId } from "../types.js";
+/**
+ * Check if two cell ids are identical.
+ *
+ * @param cellId1 - Cell id 1 to compare.
+ * @param cellId2 - Cell id 1 to compare.
+ * @returns True if the cell ids are identical.
+ *
+ * @public
+ */
+export declare const isSameCell: (cellId1: CellId, cellId2: CellId) => boolean;
